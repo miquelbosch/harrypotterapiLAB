bf355aea72f5294521a59415ccd0dddeb89c7c72
Miquel Bosch <miquel.bosch.cortes@gmail.com>
2020-11-29 20:42:35 +0100

    initial commit - add project framework + single app

---
+++ HarryPotterLab-iOS/HarryPotterLab-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterLab-iOS
+//
+//  Created by Miquel Bosch on 29/11/2020.
+//  Copyright © 2020 Miquel Bosch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
---
+++ HarryPotterLab_ui_kit/HarryPotterLab_ui_kit/HarryPotterLab_ui_kit.h
@@ -0,0 +1,19 @@
+//
+//  HarryPotterLab_ui_kit.h
+//  HarryPotterLab_ui_kit
+//
+//  Created by Miquel Bosch on 29/11/2020.
+//  Copyright © 2020 Miquel Bosch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HarryPotterLab_ui_kit.
+FOUNDATION_EXPORT double HarryPotterLab_ui_kitVersionNumber;
+
+//! Project version string for HarryPotterLab_ui_kit.
+FOUNDATION_EXPORT const unsigned char HarryPotterLab_ui_kitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HarryPotterLab_ui_kit/PublicHeader.h>
+
+
