fc67aa68897bea26fd01989d4b99dee406ed8e10
Miquel Bosch <miquel.bosch.cortes@gmail.com>
2020-11-29 21:45:08 +0100

    add test for format function

---
+++ HarryPotterLab_ui_kit/HarryPotterLab_ui_kitTests/Managers/DateManagerTest.swift
@@ -0,0 +1,36 @@
+//
+//  DateManagerTest.swift
+//  HarryPotterLab_ui_kitTests
+//
+//  Created by Miquel Bosch on 29/11/2020.
+//  Copyright © 2020 Miquel Bosch. All rights reserved.
+//
+
+import XCTest
+@testable import HarryPotterLab_ui_kit
+
+class DateManagerTest: XCTestCase {
+    
+    // MARK: - Properties
+    private var date: Date!
+    
+    // MARK: - Setup Test
+    override func setUp() {
+        super.setUp()
+        
+        let calendar = Calendar.current
+        var components = DateComponents()
+        components.day = 12
+        components.month = 12
+        components.year = 2021
+        
+        date = calendar.date(from: components)
+    }
+    
+    // MARK: - Tests
+    func test_convert_string_To_Date_ddMMyyyy() {
+        
+        let date = DateManager.toDate(from: "12/12/2021", in: .ddMMyyyy)
+        XCTAssertEqual(date, self.date)
+    }
+}
